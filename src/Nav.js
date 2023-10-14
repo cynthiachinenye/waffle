833a9208be546da764b6e5d3b6e70bb22b42b344
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-14 17:02:35 +0100

    new

--- src/Nav.js
+++ src/Nav.js
@@ -1,38 +1,46 @@
-import React from 'react'
+
 import { FaTruckMoving } from 'react-icons/fa'
-import {AiOutlineHeart} from 'react-icons/ai'
-import { BsBagCheck} from 'react-icons/bs'
+import { AiOutlineHeart, AiOutlineUser } from 'react-icons/ai'
+import { BsBagCheck } from 'react-icons/bs'
 
 import './nav.css';
 
 const Nav = () => {
     return (
         <>
-        <div className='free'>
-            <div className='icon'>
-                <FaTruckMoving />
-            </div>
-            <p> FREE Shipping when shopping upto $1000</p>
+            <div className='free'>
+                <div className='icon'>
+                    <FaTruckMoving />
+                </div>
+                <p> FREE Shipping when shopping upto $1000</p>
             </div>
             <div className='main-header'>
-           <div className='container'>
-           <div className='logo'>
-           <img src='./images/logo.png' alt='logo'/>
-           </div>
-           <div className='search-box'>
-           <input type="text" value='' placeholder='Enter The Product Name' autoComplete='off'/>
-           <button>Search</button>
-           <AiOutlineHeart/>
-           <BsBagCheck/>
+                <div className='container'>
+                    <div className='logo'>
+                        <img src='./images/logo.png' alt='logo' />
+                    </div>
+                    <div className='search-box'>
+                        <input type="text" value='' placeholder='Enter The Product Name' autoComplete='off' />
+                        <button>Search</button>
+                        </div>
+                        <div className='icon'>
+                            <div className='account'>
+                                <div className='user-icon'>
+                                    <AiOutlineUser />
+                                </div>
+                                <AiOutlineHeart />
+                                <p>Hello User</p>
+                                <BsBagCheck />
+                            </div>
+                        </div>
+                  
+                </div>
 
-           </div>
-           </div>
-        
-        </div>
+            </div>
 
-        </>  
-        
-        )
+        </>
+
+    )
 }
 
 export default Nav
