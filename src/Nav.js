4ad41a201626a6d854585d669094c813b530d638
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-14 16:43:38 +0100

    new

--- src/Nav.js
+++ src/Nav.js
@@ -1,17 +1,38 @@
 import React from 'react'
 import { FaTruckMoving } from 'react-icons/fa'
+import {AiOutlineHeart} from 'react-icons/ai'
+import { BsBagCheck} from 'react-icons/bs'
+
 import './nav.css';
 
 const Nav = () => {
     return (
-        <div className='nav'>
+        <>
+        <div className='free'>
             <div className='icon'>
                 <FaTruckMoving />
             </div>
-            <p> Free Shiping when shopping upto $1000</p>
+            <p> FREE Shipping when shopping upto $1000</p>
+            </div>
+            <div className='main-header'>
+           <div className='container'>
+           <div className='logo'>
+           <img src='./images/logo.png' alt='logo'/>
+           </div>
+           <div className='search-box'>
+           <input type="text" value='' placeholder='Enter The Product Name' autoComplete='off'/>
+           <button>Search</button>
+           <AiOutlineHeart/>
+           <BsBagCheck/>
 
+           </div>
+           </div>
+        
         </div>
-    )
+
+        </>  
+        
+        )
 }
 
 export default Nav
