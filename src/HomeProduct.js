b300d565f233255ab048c857364c3ed449970ba7
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-17 17:06:06 +0100

    new

--- src/HomeProduct.js
+++ src/HomeProduct.js
@@ -18,24 +18,24 @@ const HomeProduct = [
 
      {
         id:3,
-        Title:"Galaxy Tab s6 Lite 10.4-inch Andriod Tablet 128GB.",
-        Cat:'Tablet',
-        Price:'$723',
+        Title:"Infinix Hot 12 6.82 4GB RAM/128GB ROM Android 12 - Black",
+        Cat:'Infinix',
+        Price:'$200',
         Img:'./images/set (6).png'
      },
 
      {
         id:4,
-        Title:"Galaxy Tab s6 Lite 10.4-inch Andriod Tablet 128GB.",
-        Cat:'Tablet',
-        Price:'$723',
+        Title:"Samsung Galaxy A14 6.64GB RAM/128GB ROM Android 13 - Black",
+        Cat:'Samsung',
+        Price:'$523',
         Img:'./images/set (7).png'
      },
 
      {
         id:5,
-        Title:"Galaxy Tab s6 Lite 10.4-inch Andriod Tablet 128GB.",
-        Cat:'Tablet',
+        Title:"Hikers Smart Watch 1.8'' Waterproof Health Monitor BT CALL Fitness Tracker Notification Weather 100 Sport Modes ",
+        Cat:'Smart Watch',
         Price:'$723',
         Img:'./images/set (8).png'
      },
@@ -58,9 +58,9 @@ const HomeProduct = [
 
      {
         id:8,
-        Title:"Galaxy Tab s6 Lite 10.4-inch Andriod Tablet 128GB.",
-        Cat:'Tablet',
-        Price:'$723',
+        Title:"Infinix Smart 7 HD 6.6 2GB RAM/64GB ROM 4G Android 12 - White ",
+        Cat:'Infinix',
+        Price:'$623',
         Img:'./images/set (11).png'
      },
     
