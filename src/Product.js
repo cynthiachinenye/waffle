fd057005dcae739393ef032a3b34fb11c7353cbb
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-18 09:54:34 +0100

    new

--- src/Product.js
+++ src/Product.js
@@ -3,7 +3,23 @@ import React from 'react'
 const Product = () => {
   return (
     <div>
-      
+    <h3> # Products</h3>
+    <p>Home . products</p>
+      <div className='products'>
+      <div className='container'>
+      <div className='filter'>
+      <div className='categories'>
+      <ul>
+      <li>Tablet</li>
+      <li>Smart watch</li>
+      <li>Headphones</li>
+      <li>Camera</li>
+      <li>Gaming</li>
+      </ul>
+      </div>
+      </div>
+      </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/ProductDetail.js
@@ -0,0 +1,71 @@
+
+const ProductDetail = [
+
+    {
+        id:1,
+        Title:"Galaxy Tab s6 Lite 10.4-inch Andriod Tablet 128GB.",
+        Cat:'Tablet',
+        Price:'$723',
+        Img:'./images/tablet.png'
+     },
+ 
+     {
+         id:2,
+         Title:"Galaxy Tab s6 Lite 10.4-inch Andriod Tablet 128GB.",
+         Cat:'Tablet',
+         Price:'$723',
+         Img:'./images/set (5).png'
+      },
+ 
+      {
+         id:3,
+         Title:"Infinix Hot 12 6.82 4GB RAM/128GB ROM Android 12 - Black",
+         Cat:'Infinix',
+         Price:'$200',
+         Img:'./images/set (6).png'
+      },
+ 
+      {
+         id:4,
+         Title:"Samsung Galaxy A14 6.64GB RAM/128GB ROM Android 13 - Black",
+         Cat:'Samsung',
+         Price:'$523',
+         Img:'./images/set (7).png'
+      },
+ 
+      {
+         id:5,
+         Title:"Hikers Smart Watch 1.8'' Waterproof Health Monitor BT CALL Fitness Tracker Notification Weather 100 Sport Modes ",
+         Cat:'Smart Watch',
+         Price:'$723',
+         Img:'./images/set (8).png'
+      },
+ 
+      {
+         id:6,
+         Title:"Galaxy Tab s6 Lite 10.4-inch Andriod Tablet 128GB.",
+         Cat:'Tablet',
+         Price:'$723',
+         Img:'./images/set (9).png'
+      },
+ 
+      {
+         id:7,
+         Title:"Galaxy Tab s6 Lite 10.4-inch Andriod Tablet 128GB.",
+         Cat:'Camera',
+         Price:'$723',
+         Img:'./images/gaming (5).png'
+      },
+ 
+      {
+         id:8,
+         Title:"Infinix Smart 7 HD 6.6 2GB RAM/64GB ROM 4G Android 12 - White ",
+         Cat:'Infinix',
+         Price:'$623',
+         Img:'./images/set (11).png'
+      },
+ 
+
+]
+
+export default ProductDetail 
