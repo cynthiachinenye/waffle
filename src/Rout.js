c4e7d2e66d8020fb1ecfd7c821cd5c5c4d15331d
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-20 23:09:06 +0100

    new

--- src/Rout.js
+++ src/Rout.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import Product from './Product'
+import Cart from './Cart'
 const Rout = ({product, setProduct,detail, view, close, setClose}) => {
   return (
     <div>
       <Routes>
       <Route path="/" element={<Home detail={detail} view={view} close={close} setClose={setClose}/>}/>
       <Route path='/product' element={<Product product={product} setProduct={setProduct} detail={detail} view={view} close={close} setClose={setClose}/>}/>
+      <Route path='/cart' element={<Cart/>}/>
       </Routes>
     </div>
   )
