f938efd721c30d777b002d9c5b754e66030c8b1e
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-23 14:43:45 +0100

    new

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,45 @@
+import React,{ useState } from 'react'
 import './contact.css'
 
 const Contact = () => {
+    const [user, setUser] = useState(
+
+        {
+            Name:'',
+            Email:"",
+            Subject:"",
+            Message:"",
+        }
+    )
+    let name, value
+    const data = (e) => {
+         name= e.target.name;
+         value =e.target.value;
+         setUser({...user,[name]:value})
+    }
+     const sendData = async(e) => {
+        const {Name, Email, Subject, Message} = user
+        e.preventDefault();
+        const options ={
+            method:'POST',
+            Headers: {
+                "Content-Type" : "aplication/json"
+            },
+            body:JSON.stringify({
+                Name, Email, Subject, Message
+            })
+
+        }
+        const res = await fetch('https://e-commerce-df21b-default-rtdb.firebaseio.com/Message.json', options)
+           console.log(res)
+           if(res)
+           {
+            alert('Your Message sent')
+           }
+           else {
+            alert('An error Occured')
+           }
+     }
   return (
     <div>
       <div className='contact-container'>
@@ -8,11 +47,11 @@ const Contact = () => {
       <h2>#Contact Us</h2>
       <div className='form'>
       <form method='POST'>
-      <input type="text" name='Name' value='' placeholder='enter your full name' required/>
-      <input type="email" name='Email' value='' placeholder='enter your mail' required/>
-      <input type="text" name='Subject' value='' placeholder='enter your Subject' required/>
-      <textarea name='Message' value='' placeholder='Your Message' required></textarea>
-      <button type='submit'> Send</button>
+      <input type="text" name='Name' value={user.Name} placeholder='enter your full name' required autoComplete='off' onChange={data}/>
+      <input type="email" name='Email' value={user.Email} placeholder='enter your mail' required autoComplete='off' onChange={data}/>
+      <input type="text" name='Subject' value={user.Subject} placeholder='enter your Subject' required autoComplete='off' onChange={data}/>
+      <textarea name='Message' value={user.Message} placeholder='Your Message' required autoComplete='off' onChange={data}></textarea>
+      <button type='submit' onClick={sendData }> Send</button>
       </form>
       </div>
       </div>
