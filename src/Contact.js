431055efc536c17a900f0931aeb9b23ceaa2e616
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-23 13:45:00 +0100

    new

--- src/Contact.js
+++ src/Contact.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import 'contact.css'
 
 const Contact = () => {
   return (
     <div>
       <div className='contact-container'>
       <div className='content'>
+      <h2>#Contact Us</h2>
       <div className='form'>
       <form method='POST'>
       <input type="text" name='Name' value='' placeholder='enter your full name' required/>
       <input type="email" name='Email' value='' placeholder='enter your mail' required/>
       <input type="text" name='Subject' value='' placeholder='enter your Subject' required/>
+      <textarea name='Message' value='' placeholder='Your Message' required></textarea>
+      <button type='submit'> Send</button>
       </form>
       </div>
       </div>
